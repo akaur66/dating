a0f8464c95269dba5cc0a5ae737676c5bf4b7302
Amardip Kaur <akaur66@mail.greenriver.edu>
2020-04-27 14:33:24 -0400

    fat free setup- index page director

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//instantiate the F3 Base class
+$f3 = Base::instance();
+
+//define a default route
+//when user visits the default root(file) - ...328/dating
+//it runs the function
+$f3->route('GET /', function(){
+    //display a page called home.html
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
